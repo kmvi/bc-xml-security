ba3c5349515aaee760f223440a65cda29585029d
kmvi <kmvi@users.noreply.github.com>
2019-06-09 07:47:52 +0300

    rename solution and project files

--- samples/Properties/AssemblyInfo.cs
+++ samples/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SignedXml.Samples")]
+[assembly: AssemblyTitle("XmlSecurity.Samples")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SignedXml.Samples")]
+[assembly: AssemblyProduct("XmlSecurity.Samples")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
