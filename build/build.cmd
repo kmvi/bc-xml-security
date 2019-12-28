9a1ce47cceb8e8e84706e567d9958ad6ad5ba92b
kmvi <kmvi@users.noreply.github.com>
2019-12-28 09:44:42 +0300

    update build script

--- build/build.cmd
+++ build/build.cmd
@@ -9,11 +9,15 @@ rmdir /s /q ..\samples\obj
 rmdir /s /q ..\tests\bin
 rmdir /s /q ..\tests\obj
 mkdir lib\net40
+mkdir lib\netstandard2.0
+mkdir lib\netstandard2.1
 
 msbuild ..\XmlSecurity.sln /t:Clean /p:Configuration=Release
 msbuild ..\XmlSecurity.sln /t:Restore
 msbuild ..\XmlSecurity.sln /p:Configuration=Release
 
-copy ..\src\bin\Release\bc-xml-security.dll lib\net40\bc-xml-security.dll
+copy ..\src\bin\Release\net40\bc-xml-security.dll lib\net40\bc-xml-security.dll
+copy ..\src\bin\Release\netstandard2.0\bc-xml-security.dll lib\netstandard2.0\bc-xml-security.dll
+copy ..\src\bin\Release\netstandard2.1\bc-xml-security.dll lib\netstandard2.1\bc-xml-security.dll
 
 nuget pack -exclude build.cmd XmlSecurity.nuspec
