a434731e0a34df87cc9ca7bfb3432b49ca74a715
kmvi <kmvi@users.noreply.github.com>
2024-05-24 14:40:23 +0300

    ability to add a custom name to algorithm mappings fixes #7

--- src/CryptoHelpers.cs
+++ src/CryptoHelpers.cs
@@ -9,72 +9,91 @@ using Org.BouncyCastle.Crypto.Paddings;
 using Org.BouncyCastle.Crypto.Signers;
 using Org.BouncyCastle.Security;
 using System;
+using System.Collections.Generic;
 
 namespace Org.BouncyCastle.Crypto.Xml
 {
-    internal static class CryptoHelpers
+    public static class CryptoHelpers
     {
         private static readonly char[] _invalidChars = new char[] { ',', '`', '[', '*', '&' };
 
-        public static object CreateFromKnownName(string name) =>
-            name switch
-            {
-                "http://www.w3.org/TR/2001/REC-xml-c14n-20010315" => new XmlDsigC14NTransform(),
-                "http://www.w3.org/TR/2001/REC-xml-c14n-20010315#WithComments" => new XmlDsigC14NWithCommentsTransform(),
-                "http://www.w3.org/2001/10/xml-exc-c14n#" => new XmlDsigExcC14NTransform(),
-                "http://www.w3.org/2001/10/xml-exc-c14n#WithComments" => new XmlDsigExcC14NWithCommentsTransform(),
-                "http://www.w3.org/2000/09/xmldsig#base64" => new XmlDsigBase64Transform(),
-                "http://www.w3.org/TR/1999/REC-xpath-19991116" => new XmlDsigXPathTransform(),
-                "http://www.w3.org/TR/1999/REC-xslt-19991116" => new XmlDsigXsltTransform(),
-                "http://www.w3.org/2000/09/xmldsig#enveloped-signature" => new XmlDsigEnvelopedSignatureTransform(),
-                "http://www.w3.org/2002/07/decrypt#XML" => new XmlDecryptionTransform(),
-                "urn:mpeg:mpeg21:2003:01-REL-R-NS:licenseTransform" => new XmlLicenseTransform(),
-                "http://www.w3.org/2000/09/xmldsig# X509Data" => new KeyInfoX509Data(),
-                "http://www.w3.org/2000/09/xmldsig# KeyName" => new KeyInfoName(),
-                "http://www.w3.org/2000/09/xmldsig# KeyValue/DSAKeyValue" => new DSAKeyValue(),
-                "http://www.w3.org/2000/09/xmldsig# KeyValue/RSAKeyValue" => new RSAKeyValue(),
-                "http://www.w3.org/2000/09/xmldsig# RetrievalMethod" => new KeyInfoRetrievalMethod(),
-                "http://www.w3.org/2001/04/xmlenc# EncryptedKey" => new KeyInfoEncryptedKey(),
-                "http://www.w3.org/2000/09/xmldsig#dsa-sha1" => new DsaDigestSigner(new DsaSigner(), new Sha1Digest(), PlainDsaEncoding.Instance),
-                "System.Security.Cryptography.DSASignatureDescription" => new DsaDigestSigner(new DsaSigner(), new Sha1Digest(), PlainDsaEncoding.Instance),
-                "http://www.w3.org/2000/09/xmldsig#rsa-sha1" => SignerUtilities.GetSigner("SHA1WITHRSA"),
-                "System.Security.Cryptography.RSASignatureDescription" => SignerUtilities.GetSigner("SHA1WITHRSA"),
-                "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256" => SignerUtilities.GetSigner("SHA256WITHRSA"),
-                "http://www.w3.org/2001/04/xmldsig-more#rsa-sha384" => SignerUtilities.GetSigner("SHA384WITHRSA"),
-                "http://www.w3.org/2001/04/xmldsig-more#rsa-sha512" => SignerUtilities.GetSigner("SHA512WITHRSA"),
-                "urn:ietf:params:xml:ns:cpxmlsec:algorithms:gostr34102001-gostr3411" => SignerUtilities.GetSigner("GOST3411WITHECGOST3410"),
-                "http://www.w3.org/2001/04/xmldsig-more#gostr34102001-gostr3411" => SignerUtilities.GetSigner("GOST3411WITHECGOST3410"),
-                "urn:ietf:params:xml:ns:cpxmlsec:algorithms:gostr34102012-gostr34112012-256" => new Gost3410DigestSigner(new ECGost3410Signer(), new Gost3411_2012_256Digest()),
-                "urn:ietf:params:xml:ns:cpxmlsec:algorithms:gostr34102012-gostr34112012-512" => new Gost3410DigestSigner(new ECGost3410Signer(), new Gost3411_2012_512Digest()),
+        private static readonly Dictionary<string, Func<object>> _knownNames = new()
+        {
+            { "http://www.w3.org/TR/2001/REC-xml-c14n-20010315", () => new XmlDsigC14NTransform() },
+            { "http://www.w3.org/TR/2001/REC-xml-c14n-20010315#WithComments", () => new XmlDsigC14NWithCommentsTransform() },
+            { "http://www.w3.org/2001/10/xml-exc-c14n#", () => new XmlDsigExcC14NTransform() },
+            { "http://www.w3.org/2001/10/xml-exc-c14n#WithComments", () => new XmlDsigExcC14NWithCommentsTransform() },
+            { "http://www.w3.org/2000/09/xmldsig#base64", () => new XmlDsigBase64Transform() },
+            { "http://www.w3.org/TR/1999/REC-xpath-19991116", () => new XmlDsigXPathTransform() },
+            { "http://www.w3.org/TR/1999/REC-xslt-19991116", () => new XmlDsigXsltTransform() },
+            { "http://www.w3.org/2000/09/xmldsig#enveloped-signature", () => new XmlDsigEnvelopedSignatureTransform() },
+            { "http://www.w3.org/2002/07/decrypt#XML", () => new XmlDecryptionTransform() },
+            { "urn:mpeg:mpeg21:2003:01-REL-R-NS:licenseTransform", () => new XmlLicenseTransform() },
+            { "http://www.w3.org/2000/09/xmldsig# X509Data", () => new KeyInfoX509Data() },
+            { "http://www.w3.org/2000/09/xmldsig# KeyName", () => new KeyInfoName() },
+            { "http://www.w3.org/2000/09/xmldsig# KeyValue/DSAKeyValue", () => new DSAKeyValue() },
+            { "http://www.w3.org/2000/09/xmldsig# KeyValue/RSAKeyValue", () => new RSAKeyValue() },
+            { "http://www.w3.org/2000/09/xmldsig# RetrievalMethod", () => new KeyInfoRetrievalMethod() },
+            { "http://www.w3.org/2001/04/xmlenc# EncryptedKey", () => new KeyInfoEncryptedKey() },
+            { "http://www.w3.org/2000/09/xmldsig#dsa-sha1", () => new DsaDigestSigner(new DsaSigner(), new Sha1Digest(), PlainDsaEncoding.Instance) },
+            { "System.Security.Cryptography.DSASignatureDescription", () => new DsaDigestSigner(new DsaSigner(), new Sha1Digest(), PlainDsaEncoding.Instance) },
+            { "http://www.w3.org/2000/09/xmldsig#rsa-sha1", () => SignerUtilities.GetSigner("SHA1WITHRSA") },
+            { "System.Security.Cryptography.RSASignatureDescription", () => SignerUtilities.GetSigner("SHA1WITHRSA") },
+            { "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256", () => SignerUtilities.GetSigner("SHA256WITHRSA") },
+            { "http://www.w3.org/2001/04/xmldsig-more#rsa-sha384", () => SignerUtilities.GetSigner("SHA384WITHRSA") },
+            { "http://www.w3.org/2001/04/xmldsig-more#rsa-sha512", () => SignerUtilities.GetSigner("SHA512WITHRSA") },
+            { "urn:ietf:params:xml:ns:cpxmlsec:algorithms:gostr34102001-gostr3411", () => SignerUtilities.GetSigner("GOST3411WITHECGOST3410") },
+            { "http://www.w3.org/2001/04/xmldsig-more#gostr34102001-gostr3411", () => SignerUtilities.GetSigner("GOST3411WITHECGOST3410") },
+            { "urn:ietf:params:xml:ns:cpxmlsec:algorithms:gostr34102012-gostr34112012-256", () => new Gost3410DigestSigner(new ECGost3410Signer(), new Gost3411_2012_256Digest()) },
+            { "urn:ietf:params:xml:ns:cpxmlsec:algorithms:gostr34102012-gostr34112012-512", () => new Gost3410DigestSigner(new ECGost3410Signer(), new Gost3411_2012_512Digest()) },
 
-                // workarounds for issue https://github.com/dotnet/corefx/issues/16563
-                // remove attribute from this method when removing them
-                "http://www.w3.org/2000/09/xmldsig#sha1" => DigestUtilities.GetDigest("SHA-1"),
-                "http://www.w3.org/2001/04/xmlenc#sha256" => DigestUtilities.GetDigest("SHA-256"),
-                "http://www.w3.org/2001/04/xmldsig-more#sha384" => DigestUtilities.GetDigest("SHA-384"),
-                "SHA384" => DigestUtilities.GetDigest("SHA-384"),
-                "http://www.w3.org/2001/04/xmlenc#sha512" => DigestUtilities.GetDigest("SHA-512"),
-                "http://www.w3.org/2001/04/xmlenc#ripemd160" => DigestUtilities.GetDigest("RIPEMD-160"),
-                "MD5" => DigestUtilities.GetDigest("MD5"),
-                "urn:ietf:params:xml:ns:cpxmlsec:algorithms:gostr3411" => DigestUtilities.GetDigest("GOST3411"),
-                "http://www.w3.org/2001/04/xmldsig-more#gostr3411" => DigestUtilities.GetDigest("GOST3411"),
-                "urn:ietf:params:xml:ns:cpxmlsec:algorithms:gostr34112012-256" => DigestUtilities.GetDigest("GOST3411-2012-256"),
-                "urn:ietf:params:xml:ns:cpxmlsec:algorithms:gostr34112012-512" => DigestUtilities.GetDigest("GOST3411-2012-512"),
-                "http://www.w3.org/2001/04/xmldsig-more#hmac-md5" => MacUtilities.GetMac("HMAC-MD5"),
-                "http://www.w3.org/2001/04/xmldsig-more#hmac-sha256" => MacUtilities.GetMac("HMAC-SHA256"),
-                "http://www.w3.org/2001/04/xmldsig-more#hmac-sha384" => MacUtilities.GetMac("HMAC-SHA384"),
-                "http://www.w3.org/2001/04/xmldsig-more#hmac-sha512" => MacUtilities.GetMac("HMAC-SHA512"),
-                "http://www.w3.org/2001/04/xmldsig-more#hmac-ripemd160" => MacUtilities.GetMac("HMAC-RIPEMD160"),
-                "http://www.w3.org/2001/04/xmlenc#des-cbc" => CipherUtilities.GetCipher("DES/CBC/PKCS7Padding"),
-                "http://www.w3.org/2001/04/xmlenc#tripledes-cbc" => CipherUtilities.GetCipher("DESede/CBC/PKCS7Padding"),
-                "http://www.w3.org/2001/04/xmlenc#aes128-cbc" => new PaddedBufferedBlockCipher(new CbcBlockCipher(new RijndaelEngine(128)), new Pkcs7Padding()),
-                "http://www.w3.org/2001/04/xmlenc#kw-aes128" => new PaddedBufferedBlockCipher(new CbcBlockCipher(new RijndaelEngine(128)), new Pkcs7Padding()),
-                "http://www.w3.org/2001/04/xmlenc#aes192-cbc" => new PaddedBufferedBlockCipher(new CbcBlockCipher(new RijndaelEngine(192)), new Pkcs7Padding()),
-                "http://www.w3.org/2001/04/xmlenc#kw-aes192" => new PaddedBufferedBlockCipher(new CbcBlockCipher(new RijndaelEngine(192)), new Pkcs7Padding()),
-                "http://www.w3.org/2001/04/xmlenc#aes256-cbc" => new PaddedBufferedBlockCipher(new CbcBlockCipher(new RijndaelEngine(256)), new Pkcs7Padding()),
-                "http://www.w3.org/2001/04/xmlenc#kw-aes256" => new PaddedBufferedBlockCipher(new CbcBlockCipher(new RijndaelEngine(256)), new Pkcs7Padding()),
-                _ => null,
-            };
+            // workarounds for issue https://github.com/dotnet/corefx/issues/16563
+            // remove attribute from this method when removing them
+            { "http://www.w3.org/2000/09/xmldsig#sha1", () => DigestUtilities.GetDigest("SHA-1") },
+            { "http://www.w3.org/2001/04/xmlenc#sha256", () => DigestUtilities.GetDigest("SHA-256") },
+            { "http://www.w3.org/2001/04/xmldsig-more#sha384", () => DigestUtilities.GetDigest("SHA-384") },
+            { "SHA384", () => DigestUtilities.GetDigest("SHA-384") },
+            { "http://www.w3.org/2001/04/xmlenc#sha512", () => DigestUtilities.GetDigest("SHA-512") },
+            { "http://www.w3.org/2001/04/xmlenc#ripemd160", () => DigestUtilities.GetDigest("RIPEMD-160") },
+            { "MD5", () => DigestUtilities.GetDigest("MD5") },
+            { "urn:ietf:params:xml:ns:cpxmlsec:algorithms:gostr3411", () => DigestUtilities.GetDigest("GOST3411") },
+            { "http://www.w3.org/2001/04/xmldsig-more#gostr3411", () => DigestUtilities.GetDigest("GOST3411") },
+            { "urn:ietf:params:xml:ns:cpxmlsec:algorithms:gostr34112012-256", () => DigestUtilities.GetDigest("GOST3411-2012-256") },
+            { "urn:ietf:params:xml:ns:cpxmlsec:algorithms:gostr34112012-512", () => DigestUtilities.GetDigest("GOST3411-2012-512") },
+            { "http://www.w3.org/2001/04/xmldsig-more#hmac-md5", () => MacUtilities.GetMac("HMAC-MD5") },
+            { "http://www.w3.org/2001/04/xmldsig-more#hmac-sha256", () => MacUtilities.GetMac("HMAC-SHA256") },
+            { "http://www.w3.org/2001/04/xmldsig-more#hmac-sha384", () => MacUtilities.GetMac("HMAC-SHA384") },
+            { "http://www.w3.org/2001/04/xmldsig-more#hmac-sha512", () => MacUtilities.GetMac("HMAC-SHA512") },
+            { "http://www.w3.org/2001/04/xmldsig-more#hmac-ripemd160", () => MacUtilities.GetMac("HMAC-RIPEMD160") },
+            { "http://www.w3.org/2001/04/xmlenc#des-cbc", () => CipherUtilities.GetCipher("DES/CBC/PKCS7Padding") },
+            { "http://www.w3.org/2001/04/xmlenc#tripledes-cbc", () => CipherUtilities.GetCipher("DESede/CBC/PKCS7Padding") },
+            { "http://www.w3.org/2001/04/xmlenc#aes128-cbc", () => new PaddedBufferedBlockCipher(new CbcBlockCipher(new RijndaelEngine(128)), new Pkcs7Padding()) },
+            { "http://www.w3.org/2001/04/xmlenc#kw-aes128", () => new PaddedBufferedBlockCipher(new CbcBlockCipher(new RijndaelEngine(128)), new Pkcs7Padding()) },
+            { "http://www.w3.org/2001/04/xmlenc#aes192-cbc", () => new PaddedBufferedBlockCipher(new CbcBlockCipher(new RijndaelEngine(192)), new Pkcs7Padding()) },
+            { "http://www.w3.org/2001/04/xmlenc#kw-aes192", () => new PaddedBufferedBlockCipher(new CbcBlockCipher(new RijndaelEngine(192)), new Pkcs7Padding()) },
+            { "http://www.w3.org/2001/04/xmlenc#aes256-cbc", () => new PaddedBufferedBlockCipher(new CbcBlockCipher(new RijndaelEngine(256)), new Pkcs7Padding()) },
+            { "http://www.w3.org/2001/04/xmlenc#kw-aes256", () => new PaddedBufferedBlockCipher(new CbcBlockCipher(new RijndaelEngine(256)), new Pkcs7Padding()) },
+        };
+
+        public static void SetKnownName<T>(string name)
+            where T : new()
+        {
+            SetKnownName(name, () => new T());
+        }
+
+        public static void SetKnownName(string name, Func<object> factory)
+        {
+            if (name == null)
+                throw new ArgumentNullException(nameof(name));
+
+            if (factory == null)
+                throw new ArgumentNullException(nameof(factory));
+
+            _knownNames[name] = factory;
+        }
+
+        public static object CreateFromKnownName(string name) =>
+            _knownNames.TryGetValue(name, out Func<object> factory) ? factory() : null;
 
         public static T CreateFromName<T>(string name) where T : class
         {
---
+++ tests/CryptoHelpersTests.cs
@@ -0,0 +1,34 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// See the LICENSE file in the project root for more information
+//
+
+using System;
+using System.Xml;
+using Xunit;
+
+namespace Org.BouncyCastle.Crypto.Xml.Tests
+{
+    public class CryptoHelpersTests
+    {
+        private class CustomTransform : Transform
+        {
+            public override Type[] InputTypes => throw new NotImplementedException();
+            public override Type[] OutputTypes => throw new NotImplementedException();
+            public override object GetOutput() => throw new NotImplementedException();
+            public override object GetOutput(Type type) => throw new NotImplementedException();
+            public override void LoadInnerXml(XmlNodeList nodeList) => throw new NotImplementedException();
+            public override void LoadInput(object obj) => throw new NotImplementedException();
+            protected override XmlNodeList GetInnerXml() => throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void CreateCustomTransformFromKnownName()
+        {
+            CryptoHelpers.SetKnownName<CustomTransform>("urn:custom-transform");
+            object result = CryptoHelpers.CreateFromKnownName("urn:custom-transform");
+
+            Assert.NotNull(result);
+            Assert.IsType<CustomTransform>(result);
+        }
+    }
+}
